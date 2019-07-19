fead0f13b8deb7266d6b7ffd6f049a2f11f15669
Chidera Anichebe <chidexy67@gmail.com>
2019-07-19 17:23:22 +0100

    Add files via upload
    This is the first Javascript task on here, they would be more tasks in the future. Guidelines and rules on how the task should be completed is contained in the code...

---
+++ BasicCalculator.js
@@ -0,0 +1,45 @@
+/*Hello, this is a program that is to take two integers from the user and perform basic arithmetic operations (addition, multiplication, division and subtaction)
+on them and output the result. There are lots of bug in the code. FIND THEM AND FIX THEM!
+
+YOU MAY NOT MODIFY THE CODE TOTALLY TO IMPLEMENT AN EASIER SYNTAX :-)
+*/
+
+feeling = prompt("Hello, how are you doing today?");
+name = prompt("That's great, we hope for better days. What's your name?");
+alert("Hey, "+name+" it's a great name you have there!");
+alert("I'm your assistant and I'm here to help you solve arithmetic problems!");
+oprt = prompt("What operation would you love to carry out today? Please enter the arithmetic symbols");
+if (oqrt != "+'||"-"|"/"||'*") {
+	alert("Invalid input");
+} elif {
+	a = prompt("Please enter a number: ");
+	b = prompt("Enter another number: ");
+}
+if (oprt = '+') {
+	add();
+}
+if (oprt = "-") {
+	subtract();
+}
+if (opit = "*") {
+	multiply();
+}
+if (oprt = "/") {
+	divislon();
+}
+func add(a,b) {
+		alert(a + b);
+		alert(a+b);
+    }
+func subtract(a,b) {
+	alert(a - b);
+	console.log(a-b);
+}
+func multiply(a, b) {
+	alert(a * b);
+	console.log(a*b);
+}
+func division(a, b) {
+	alert(a / b);
+	console.log(a/b);
+}
